d875ab811740113fa4ad9521c62096d6d8ee4d41
Erik van Kempen <erik@inclino.nl>
2013-08-05 15:54:23 +0200

    SEM modeling option filled
    SEM modeling option filled, minor change in VAR model (added a missing
    parameter)

--- R/cem.R
+++ R/cem.R
@@ -1,4 +1,11 @@
-"ce.model" <- function(x, 
+"ce.model" <- function(x,
+                sem.method = "OLS",
+                sem.inst = NULL,
+                sem.data = NULL,
+                sem.restrict.matrix = NULL,
+                sem.restrict.rhs = NULL,
+                sem.restrict.regMat = NULL,
+                sem.pooled = FALSE,
                 var.p = 1,
                 var.type = "const",
                 var.season = NULL,
@@ -6,8 +13,6 @@
                 var.lag.max = NULL,
                 var.ic = NULL,
                 ...) {
-  
-  x <- as.matrix(x)
   
   # Sanity checks
   if(any(is.na(x))) {
@@ -17,12 +22,18 @@
    
   ##  Models
   # SEM
-  model.sem <- NULL
+  require("systemfit")
+  sem.formulas <- as.list(NULL)
+  for (col in seq(ncol(x))) {
+    sem.formula = paste("x$",dimnames(x)[[2]][col], " ~ ", paste(paste("x$", dimnames(x)[[2]]), collapse= "+"))
+    sem.formulas = c(sem.formulas, as.formula(sem.formula))
+  }
+  model.sem <- systemfit(sem.formulas, method = sem.method, inst = sem.inst, data = sem.data, restrict.matrix = sem.restrict.matrix, restrict.rhs = sem.restrict.rhs, restrict.regMat = sem.restrict.regMat, pooled = sem.pooled)
    
   # VAR
-  require(vars)
+  require("vars")
   x.ts <- as.ts(x)
-  model.var <- VAR(x.ts, p = var.p, type = var.type, season = var.season, lag.max = var.lag.max, ic = var.ic)
+  model.var <- VAR(x.ts, p = var.p, type = var.type, season = var.season, exogen = var.exogen, lag.max = var.lag.max, ic = var.ic)
   
   # RVAR
   model.rvar <- NULL
